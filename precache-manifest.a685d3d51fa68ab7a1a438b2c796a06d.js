b350d47beb850d43a4c1d8a58a58bebbbc51b1a2
Norbis Magallanes <norbis.magallanes@attlaris.pe>
2020-07-04 17:13:01 -0500

    fisrt commit

---
+++ precache-manifest.a685d3d51fa68ab7a1a438b2c796a06d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d32f290cf297d2840704ce67fd1bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc46bc91b61e9359216",
+    "url": "/static/css/main.2b4cd103.chunk.css"
+  },
+  {
+    "revision": "53503ef9ab1e5aaf3eb1",
+    "url": "/static/js/2.afa2088d.chunk.js"
+  },
+  {
+    "revision": "3eca2f412586d1bd3b60f3d0602bf622",
+    "url": "/static/js/2.afa2088d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc46bc91b61e9359216",
+    "url": "/static/js/main.0ac2b764.chunk.js"
+  },
+  {
+    "revision": "8da274db600c6dbac3b5",
+    "url": "/static/js/runtime-main.a7b52c36.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "d75d794a8c93274803b8d0928634791f",
+    "url": "/static/media/bg.d75d794a.png"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "f720d4ea192956d4051b73b128580704",
+    "url": "/static/media/homeback.f720d4ea.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "a0b624f2140044ec2275550595a3e4cf",
+    "url": "/static/media/userPortada.a0b624f2.png"
+  }
+]);
